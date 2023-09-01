c4df295aad3a5d886a7ade244117904caca42e98
ThiagoDefini <thiagod.scherer@hotmail.com>
2023-09-01 14:52:22 -0300

    Finalizado 2

--- SwiftfulMapApp/Views/LocationDetailView.swift
+++ SwiftfulMapApp/Views/LocationDetailView.swift
@@ -87,7 +87,7 @@ extension LocationDetailView{
     }
     
     private var mapLayer: some View {
-        Map(coordinateRegion: .constant(MKCoordinateRegion(center: location.coordinates, span: vm.mapSpan)),
+        Map(coordinateRegion: .constant(MKCoordinateRegion(center: location.coordinates, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))),
             annotationItems: [location]) { location in
             MapAnnotation(coordinate: location.coordinates) {
                 LocationMapAnnotationView()
